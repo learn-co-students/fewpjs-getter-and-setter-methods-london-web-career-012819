d10a4b49679e8e04b2d77e15649b06b40cdeae8a
github username <github email address>
2019-03-17 19:53:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Add your Circle class here
+class Circle {
+  constructor(radius) {
+    this.radius = radius
+  }
+
+  get diameter() {
+    return this.radius * 2;
+  }
+
+  set diameter(diameter) {
+    this.radius = diameter / 2;
+  }
+
+  get circumference() {
+    return (2 * Math.PI) * this.radius;
+  }
+
+  set circumference(circumference) {
+    this.radius = circumference / (Math.PI * 2);
+  }
+
+  get area() {
+    return Math.PI * (this.radius * this.radius);
+  }
+
+  set area(area) {
+    this.radius = (Math.sqrt(area)) / Math.PI;
+  }
+}
+
+// class Bird {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//
+//   set phrase(phrase) {
+//     this._phrase = phrase;
+//   }
+//
+//   get speak() {
+//     return `${this.name} says ${this._phrase || 'squawk'}`;
+//   }
+// }
+//
+// let daffy = new Bird('Daffy');
